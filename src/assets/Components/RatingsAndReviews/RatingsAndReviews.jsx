00cbb564b71efcfc576f8625acc607113636d707
Your Name <mdshahreerirfan@gmail.com>
2024-03-05 02:04:31 +0600

    [Added] RatingsAndReviews component added

---
+++ src/assets/Components/RatingsAndReviews/RatingsAndReviews.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+const RatingsAndReviews = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default RatingsAndReviews;
